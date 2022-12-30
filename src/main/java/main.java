510e142bca3e9f561eb7a65d5bab654f91a52bee
Viktor <ren1399@yandex.ru>
2022-12-30 12:10:40 +0500

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        int expected = 150;
+        long actiual = service.calculate(5_000, true);
+        System.out.println("1. " + expected + " == ? == " + actiual);
+
+    }
+}
